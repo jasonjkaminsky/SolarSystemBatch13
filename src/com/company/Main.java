d98a93ee6b408c399d41d7fe3aa66e5f09a95fab
jasonjkaminsky <jasonjkaminsky@gmail.com>
2022-08-28 10:15:57 -0500

    Feature for planet 2 has been added with 0 errors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,6 +15,14 @@ public class Main {
         omaro.name="ferro";
         omaro.radius=1234455;
         omaro.color="pink";
+
+        //  Developing Planet2
+        Planet2 superwomen = new Planet2();
+        superwomen.size="766585857km";
+        superwomen.name="pliea";
+        superwomen.color="green";
+
+
     }
 
 }
